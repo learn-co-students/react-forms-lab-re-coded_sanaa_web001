0b862a4b3179a24cef485072596d19764de6596c
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-09-06 17:27:50 +0300

    Done!

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,39 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
   }
+
+  handleMessageChange = (event) => {
+    this.setState({
+      message: event.target.value,
+    });
+  };
+
+  remainingChars = (message, charsNumbers) => {
+    return charsNumbers - message.split("").length;
+  };
+
+  remainingCharsBox = (remainingChars) => {
+    return <div>{remainingChars}</div>;
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={(event) => this.handleMessageChange(event)}
+        />
+        {this.remainingCharsBox(
+          this.remainingChars(this.state.message, this.props.maxChars)
+        )}
       </div>
     );
   }
