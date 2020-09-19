7e060819b0cc194e2b71fcd17af69f97bf7f4593
OsamahGharad <osamah.gharred@gmail.com>
2020-09-20 00:49:52 +0300

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:" ",
+      password: " "
+    };
   }
+handlChangeName = event =>{
+  this.setState( {
+    username:event.target.value
+  })
+}
 
+handlChangePass = event =>{
+  this.setState( {
+    password:event.target.value
+  })
+}
+handleSubmit =event =>{
+   event.preventDefault();
+   if(this.state.username !=="" && this.state.password !==""){
+     this.props.handleLogin(this.state.username,this.state.password);
+   }
+   else return
+}
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handlChangeName} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handlChangePass} value={this.state.username} />
           </label>
         </div>
         <div>
@@ -31,3 +51,15 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+
+
+// 4) <LoginForm /> Saving input values in state should save the username in state when the input changes:
+     
+//   5) <LoginForm /> Saving input values in state should save the password in state when the input changes:
+    
+
+//   6) <LoginForm /> Calling `handleLogin` callback prop should call the prevent the default action when the form is being submitted:
+     
+
+//   7) <LoginForm /> Calling `handleLogin` callback prop should call the `handleLogin` callback prop when the form is being submitted:
+     
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,33 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message:"",
+      remainingChars:0
+    };
   }
-
+  twittHandler=(event)=>{
+    // console.log(event.target.value.length);
+    if(event.target.value.length<=this.props.maxChars){
+      this.setState(({message:event.target.value,
+      remainingChars:event.target.value.length}))
+      
+    }
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+        <input type="text" name="message" id="message" onChange={this.twittHandler} value={this.state.message}/>
+        <p>Remaining chars: {this.props.maxChars-this.state.remainingChars}</p>
+        </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+
+{/* <TwitterMessage /> Saving input value in state should update the state when typing:
+<TwitterMessage /> Character counter should show the remaining characters counter:
+ <TwitterMessage /> Character counter should update the counter when typing: */}
